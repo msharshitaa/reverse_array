95fc942072135c5f300a29b2d7c0156bc2e31b61
Harshitaa M S <88045719+msharshitaa@users.noreply.github.com>
2023-07-13 20:48:24 -0700

    Add files via upload

---
+++ reversearray.py
@@ -0,0 +1,11 @@
+def reverse(arr):
+    i=0
+    j=len(arr)-1
+    while i<j:
+        arr[i],arr[j]=arr[j],arr[i]
+        i+=1
+        j-=1
+    return arr
+
+arr=list(map(int,input().split()))
+print(reverse(arr))
